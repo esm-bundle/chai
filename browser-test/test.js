c7216a5a0553f4d1f84ca119e3f0f68700815b57
Lars den Bakker <larsdenbakker@gmail.com>
2020-08-21 20:11:20 +0200

    Setup

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,9 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/chai", () => {
   it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
+    return import("/base/esm/chai.js");
   });
 
   it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+    return System.import("/base/system/chai.js");
   });
 });
